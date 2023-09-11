f123edf873008d041cb1feb057257c6550e5844b
TimMarcelino <valentim.marcelino@live.com>
2023-09-11 00:38:17 -0300

    Couting Sort

---
+++ FASE-1-NIVEL-1/coutingSort.rb
@@ -0,0 +1,4 @@
+# A classificação por contagem é uma técnica de classificação baseada em chaves entre um
+# intervalo específico. Funciona contando o número de objetos com valores-chave distintos
+# Nessa implementação todos os elementos do array que vamos ordenar são inteiros positivos
+# não há repetição de elementos no array que vamos ordenar;
