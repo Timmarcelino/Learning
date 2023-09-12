ac87ecdfeeeb60fcde4d5d7674ee24f6b4069026
TimMarcelino <valentim.marcelino@live.com>
2023-09-12 13:15:58 -0300

    script minimum_sum 1

---
+++ FASE-1-NIVEL-1/minimum_sum_Particular.rb
@@ -0,0 +1,13 @@
+## Soma mínima escolhendo o mínimo de pares da matriz
+## Solução criada a partir da solicitação:
+#Dado um array A[] de n elementos. Precisamos selecionar dois elementos adjacentes e excluir o maior deles e armazenar o menor.Precisamos realizar esta operação até que o array A[] contenha apenas um único elemento. Finalmente, temos que construir um array  de tal forma que a soma total do seu elemento seja mínima.
+
+def minSum(array)
+
+end
+
+## TESTE
+
+arr = [7, 2, 3, 4, 5, 6]
+puts minSum(arr).inspect
+#Resposta esperada 5 pois(2+3)
