bfbf346527ec01cc0074747f26319ce66ea64144
Timmarcelino <valentim.marcelino@live.com>
2023-09-13 16:03:29 -0300

    Building: Solução melhorada

--- FASE-1-NIVEL-1/min_product.rb
+++ FASE-1-NIVEL-1/min_product.rb
@@ -1,20 +1,39 @@
 ## O subconjunto de produto mínimo de uma matriz refere-se a um subconjunto de elementos da matriz de modo que o
 ## produto dos elementos do subconjunto seja o menor possível. Para encontrar o subconjunto mínimo de produtos,
 ## vários algoritmos podem ser usados, como greedy algorithms, dynamic programming e branch and bound.
-## Implementaremos uma abordagem simplificada de branch and bound
 
-def minProduct(arr)
-  n = arr.length
-  selected_indices = []
+def min_product_subset(arr)
+  positive_numbers = []
+  negative_numbers = []
+  zero_count = 0
 
+  arr.each do |num|
+    if num > 0
+      positive_numbers << num
+    elsif num < 0
+      negative_numbers << num
+    else
+      zero_count += 1
+    end
+  end
+
+  negative_numbers.sort_by! { |num| num.abs } # Ordena os números negativos em ordem crescente pelo valor absoluto
+
+  if positive_numbers.any? # Se houver um número positivo, inclua-o, pois multiplicar por 1 não afeta o produto
+    negative_numbers.pop
+  end
+
+  # Calcula o produto dos elementos escolhidos
+  result = positive_numbers.reduce(1, :*) * negative_numbers.reduce(1, :*)
+
+  # Se houver zeros no array original, o produto final é 0
+  if zero_count > 0
+    result = 0
+  end
+
+  result
 end
 
-# Testes
-array = [ -1, -1, -2, 4, 3 ]
-puts minProduct(array)
-
-array = [ -1, 0 ]
-puts minProduct(array)
-
-array = [ 2, -3, 1, 0, -5 ]
-puts minProduct(array)
+# Teste
+array = [-1, -1, -2, 4, 3]
+puts min_product_subset(array)
