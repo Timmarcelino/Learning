42225ad9e4f870687b8f397923b38d30598af82d
Timmarcelino <valentim.marcelino@live.com>
2023-09-13 15:30:24 -0300

    Minimum Product Abordagem simplificada de branch and bound Fixes #7

---
+++ FASE-1-NIVEL-1/min_product.rb
@@ -0,0 +1,4 @@
+## O subconjunto de produto mínimo de uma matriz refere-se a um subconjunto de elementos da matriz de modo que o
+## produto dos elementos do subconjunto seja o menor possível. Para encontrar o subconjunto mínimo de produtos,
+## vários algoritmos podem ser usados, como greedy algorithms, dynamic programming e branch and bound.
+## Implementaremos uma abordagem simplificada de branch and bound
