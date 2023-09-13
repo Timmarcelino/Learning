111e55a968a437334f5bdb68de24293f8fc2f6bc
Timmarcelino <valentim.marcelino@live.com>
2023-09-13 15:34:00 -0300

    Building: minProduct

--- FASE-1-NIVEL-1/min_product.rb
+++ FASE-1-NIVEL-1/min_product.rb
@@ -2,3 +2,19 @@
 ## produto dos elementos do subconjunto seja o menor possível. Para encontrar o subconjunto mínimo de produtos,
 ## vários algoritmos podem ser usados, como greedy algorithms, dynamic programming e branch and bound.
 ## Implementaremos uma abordagem simplificada de branch and bound
+
+def minProduct(arr)
+  n = arr.length
+  selected_indices = []
+
+end
+
+# Testes
+array = [ -1, -1, -2, 4, 3 ]
+puts minProduct(array)
+
+array = [ -1, 0 ]
+puts minProduct(array)
+
+array = [ 2, -3, 1, 0, -5 ]
+puts minProduct(array)
