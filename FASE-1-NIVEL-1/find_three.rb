1c5bb62463500a5396045fdf7398c0bc66a093e5
Timmarcelino <valentim.marcelino@live.com>
2023-09-14 14:59:55 -0300

    TEST: Find 3 Largest

--- FASE-1-NIVEL-1/find_three.rb
+++ FASE-1-NIVEL-1/find_three.rb
@@ -1,26 +1,36 @@
 ## Dado um array com todos os elementos distintos, encontre os três maiores elemento
 def find3maiores(array)
-  if array.length < 3
-    return false
-  elsif array.length == 3
+  if array.length <= 3
     return array
   else
     first = -1.0/0.0 #infinito negativo (número muito, muito pequeno)
-    secund = -1.0/0.0
+    second  = -1.0/0.0
     third = -1.0/0.0
     array.each do |num|{
       if num > first
-        third = secund
-        secund = first
+        third = second
+        second  = first
         first = num
-      elsif num > secund
-        third = secund
-        secund = num
+      elsif num > second
+        third = second
+        second  = num
       elsif num > third
         third = num
       end
     }
-    return {First: first, Secund: secund, |Third: third}
     end
+    return {First: first, Second : second , Third: third}
   end
 end
+############################
+
+array = [1, 2 ,3]
+7.times do
+  rnd = rand(1..100)
+  unless array.include?(rnd)
+    array << rnd
+  end
+end
+puts array.inspect
+resultado = find3maiores(array)
+puts "\n Os três maiores elementos são: #{resultado[:First]}, #{resultado[:Second]} e #{resultado[:Third]}\n"
