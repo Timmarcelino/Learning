02f86bc3c8ff661e31659534c08941b616c03bba
Timmarcelino <valentim.marcelino@live.com>
2023-09-14 14:09:43 -0300

    REFACTOR: Counting + Clareza Nomes mais descritivos. Introdução ao uso do case

--- FASE-1-NIVEL-1/Count.rb
+++ FASE-1-NIVEL-1/Count.rb
@@ -1,30 +1,33 @@
 ## Contar letras maiúsculas, minúsculas, caracteres especiais e valores numéricos
 
-def count(frase)
-  upper = 0
-  lower = 0
-  number = 0
-  special = 0
+def count_characters(frase)
+  upper_count = 0
+  lower_count = 0
+  number_count = 0
+  special_count = 0
+
   frase = frase.to_s
-  if frase.length > 0
-    for e in frase.chars do
-      if e.ord >= 65 and e.ord <=90
-        upper += 1
-      elsif e.ord >= 97 and e.ord <=122
-        lower += 1
-      elsif e.ord >= 48 and e.ord <=57
-        number += 1
-      else
-        special += 1
-      end
+
+  frase.each_char do |char|
+    case char
+    when 'A'..'Z'
+      upper_count += 1
+    when 'a'..'z'
+      lower_count += 1
+    when '0'..'9'
+      number_count += 1
+    else
+      special_count += 1
     end
   end
-  resposta = {Upper: upper, Lower: lower, Number: number, Special: special}
+
+  resposta = { Upper: upper_count, Lower: lower_count, Number: number_count, Special: special_count }
   return resposta
 end
+
 ################################################
 #Testes
 print "Digite uma frase: "
 frase = gets.chomp
-resultado = count(frase.to_s)
+resultado = count_characters(frase.to_s)
 puts "\n Maiúsculas =  #{resultado[:Upper]}\n Minúsculas = #{resultado[:Lower]}\n Números = #{resultado[:Number]}\n Caracteres especiais = #{resultado[:Special]}\n"
