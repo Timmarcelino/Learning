b7d88ea596f4a36d6f932bd00d8d1523580c6c40
Timmarcelino <valentim.marcelino@live.com>
2023-09-14 15:03:10 -0300

    REFACTOR: Melhorias

--- FASE-1-NIVEL-1/find_three.rb
+++ FASE-1-NIVEL-1/find_three.rb
@@ -3,10 +3,10 @@ def find3maiores(array)
   if array.length <= 3
     return array
   else
-    first = -1.0/0.0 #infinito negativo (número muito, muito pequeno)
+    first = -1.0/0.0  #infinito negativo (número muito, muito pequeno)
     second  = -1.0/0.0
     third = -1.0/0.0
-    array.each do |num|{
+    array.each do |num|
       if num > first
         third = second
         second  = first
@@ -17,20 +17,19 @@ def find3maiores(array)
       elsif num > third
         third = num
       end
-    }
     end
-    return {First: first, Second : second , Third: third}
+    return {First: first, Second: second, Third: third}
   end
 end
 ############################
 
 array = [1, 2 ,3]
 7.times do
-  rnd = rand(1..100)
+  rnd = rand(-100..100)
   unless array.include?(rnd)
     array << rnd
   end
 end
 puts array.inspect
 resultado = find3maiores(array)
-puts "\n Os três maiores elementos são: #{resultado[:First]}, #{resultado[:Second]} e #{resultado[:Third]}\n"
+puts "\nOs três maiores elementos são: #{resultado[:First]}, #{resultado[:Second]} e #{resultado[:Third]}\n"
