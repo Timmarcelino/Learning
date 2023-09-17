dcae3fa570a188e9f8e5eea0fb680f6d4ebe6f3d
TimMarcelino <valentim.marcelino@live.com>
2023-09-17 04:53:53 -0300

    TESTE: Calculate Age

--- FASE-1-NIVEL-1/Calculate_age.rb
+++ FASE-1-NIVEL-1/Calculate_age.rb
@@ -1,6 +1,31 @@
 ## Tendo uma a data fornecida e a data de nascimento, encontre a idade para a data fornecida.
 
+require 'date'
+def calculateAge( dtNascimento, dtFornecida)
+    formatDate = "%d/%m/%Y"
+    nasc = Date.strptime(dtNascimento, formatDate)
+    dataInformada = Date.strptime(dtFornecida, formatDate)
 
+    anos = (dataInformada - nasc).to_i / 365
+    if nasc.mday > dataInformada.mday
+        dias = (dataInformada - Date.new((dataInformada -31).year, (dataInformada-31).mon, nasc.mday)).to_i
+        if nasc.mon > dataInformada.mon
+            meses = (12 - nasc.mon) + dataInformada.mon
+        elsif nasc.mon == dataInformada.mon
+            meses = 11
+        else
+            meses = dataInformada.mon - nasc.mon
+        end
+    else
+        dias = dataInformada.mday - nasc.mday
+        if nasc.mon > dataInformada.mon
+            meses = (12 - nasc.mon) + dataInformada.mon + 1
+        else
+            meses = dataInformada.mon - nasc.mon + 1
+        end
+    end
+    return {Anos: anos, Meses: meses, Dias: dias}
+end
 
 ###################################
 #Testes
@@ -17,4 +42,6 @@ anoI = '2023'
 
 dataNascimento = diaN.to_i.to_s + '/' + mesN.to_i.to_s + '/' + anoN.to_i.to_s
 dataInserida = diaI.to_i.to_s + '/' + mesI.to_i.to_s + '/' + anoI.to_i.to_s
-calculateAge(dataNascimento, dataInserida)
+resposta = calculateAge(dataNascimento, dataInserida)
+
+puts "A idade de acordo com os dados fornecidos são #{resposta}"
