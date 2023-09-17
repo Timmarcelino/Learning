7721f086548f29ab0ef6df230eccf65d6d70bda2
TimMarcelino <valentim.marcelino@live.com>
2023-09-17 00:10:15 -0300

    FEAT: Binary Search Fixes #33

---
+++ FASE-1-NIVEL-1/Binary_Search.rb
@@ -0,0 +1,4 @@
+##  A ideia fundamental por trás da busca binária é dividir repetidamente a lista pela metade e
+## descartar uma metade com base na comparação do elemento procurado com o elemento no meio da lista.
+## Esse processo é repetido até que o elemento desejado seja encontrado ou até que seja determinado
+## que o elemento não está na lista. O maior problema é ordenar a lista antes.
