b895cff699a36469fa996ec642302262f7ce8789
TimMarcelino <valentim.marcelino@live.com>
2023-09-17 03:59:55 -0300

    FEAT: Find Future Fixes #16

---
+++ FASE-1-NIVEL-1/Find_Future.rb
@@ -0,0 +1,5 @@
+## Dado um array arr[] consistindo em N strings e um array Query[] consistindo em Q consultas. 
+## Cada string nas matrizes arr[] e Query[] tem o formato D/M/Y onde D, M e Y denotam a data, 
+## mês e ano. Para cada consulta, a tarefa é imprimir a próxima data mais próxima do array 
+## fornecido arr[]. Se tal data não existir, imprima “-1”
+
