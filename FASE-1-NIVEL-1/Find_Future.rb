dfab71347dc155a053178f1e05d25fbb03bcf4d5
TimMarcelino <valentim.marcelino@live.com>
2023-09-17 04:02:42 -0300

    Feat: Find_Future

--- FASE-1-NIVEL-1/Find_Future.rb
+++ FASE-1-NIVEL-1/Find_Future.rb
@@ -2,4 +2,20 @@
 ## Cada string nas matrizes arr[] e Query[] tem o formato D/M/Y onde D, M e Y denotam a data, 
 ## mês e ano. Para cada consulta, a tarefa é imprimir a próxima data mais próxima do array 
 ## fornecido arr[]. Se tal data não existir, imprima “-1”
+#######################################
 
+def find_future(array, pesquisa)
+    
+end
+
+
+
+#######################################
+#Testes
+
+arr = ['22/4/1233', '1/3/633', '23/5/56645', '4/12/233']
+puts arr.inspect
+#query = ["23/3/4345", "12/3/2"]
+query = ["4/4/34234234"]
+puts query.inspect
+find_future(arr, query)
