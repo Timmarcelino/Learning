d309e3dcc06d8730239bf07932e897773efb1e20
Timmarcelino <valentim.marcelino@live.com>
2023-09-19 17:01:43 -0300

    REFACTOR: jump_search Melhorias e testes Finais
    Finish

--- FASE-2-NIVEL-2/jump_search.rb
+++ FASE-2-NIVEL-2/jump_search.rb
@@ -2,31 +2,29 @@
 ## que contém o valor procurado. Uma vez que o bloco relevante é encontrado, uma busca linear é realizada dentro desse 
 ## bloco para localizar o elemento específico.
 
-def jump_search(array, alvo)
-    n = array.length
+def jump_search(arr, target)
+    n = arr.length
     step = Math.sqrt(n).to_i
+    prev = 0
   
     # Encontre o bloco onde o elemento alvo pode estar
-    prev = 0
-    while array[min(step, n) - 1] < alvo
+    while arr[[step, n].min - 1] < target
       prev = step
       step += Math.sqrt(n).to_i
       return -1 if prev >= n # Elemento não encontrado
     end
   
-    # Faça uma busca linear no bloco
-    while array[prev] < alvo
+    # Faça uma busca linear dentro do bloco
+    while arr[prev] < target
+      return -1 if prev == [step, n].min - 1 # Elemento não encontrado
       prev += 1
-      return -1 if prev == [step, n].min # Elemento não encontrado
     end
   
-    # Se o elemento for encontrado, retorne o índice
-    return prev if array[prev] == alvo
-  
-    # Elemento não encontrado
-    return -1
+    return prev if arr[prev] == target
+    return -1 # Elemento não encontrado
   end
   
+  ####################################################
   # Teste:
   
   array = [1, 3, 5, 7, 9, 11, 13, 15]
@@ -40,3 +38,4 @@ def jump_search(array, alvo)
     puts "O elemento #{alvo_element} não foi encontrado na lista."
   end
   
+  ####
