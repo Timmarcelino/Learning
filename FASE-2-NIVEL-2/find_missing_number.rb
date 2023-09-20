a5e85c7cdc2b68912ab86e7b63a1f700345414ad
TimMarcelino <valentim.marcelino@live.com>
2023-09-20 16:28:38 -0300

    FEAT: find-missing-number

---
+++ FASE-2-NIVEL-2/find_missing_number.rb
@@ -0,0 +1,6 @@
+## Dado um array arr[] de tamanho N-1 com inteiros no intervalo de [1, N], a tarefa é encontrar o 
+## número que falta nos primeiros N inteiros
+
+def find_missing_number(arr)
+    
+end
