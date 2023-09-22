dab26beceba48452976a4c35969d9ceb5c5e1c50
TimMarcelino <valentim.marcelino@live.com>
2023-09-22 14:26:34 -0300

    TEST: Quick Sort

--- FASE-2-NIVEL-2/quick_sort.rb
+++ FASE-2-NIVEL-2/quick_sort.rb
@@ -5,3 +5,43 @@
 ## A partição é feita recursivamente em cada lado do pivô depois que o pivô é colocado em sua 
 ## posição correta e isso finalmente classifica o array.
 
+def quick_sort(arr)
+  quick_sort_helper(arr, 0, arr.length - 1)
+  return arr
+end
+
+def quick_sort_helper(arr, low, high)
+    if low < high
+        pivot_index = partition(arr, low, high)  # Particiona o array e obtém o índice do pivô
+        # Ordenar recursivamente as partições à esquerda e à direita do pivô
+        quick_sort_helper(arr, low, pivot_index - 1)
+        quick_sort_helper(arr, pivot_index + 1, high)
+    end
+end
+
+def partition(arr, low, high)
+    pivot = arr[high]  # Escolhe o último elemento como pivô
+    i = low - 1 # Inicializa o índice do menor elemento
+
+    (low..high -1).each do |j|  # Percorre o array e move os elementos menores que o pivô para a esquerda
+        if arr[j] < pivot
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+        end
+    end
+    arr[i+1], arr[high] = arr[high], arr[i+1] # Move o pivô para a posição correta
+    
+    return i+1 # Retorna o índice do pivô
+end
+
+###########
+#Teste
+
+array =[]
+7.times{
+  rnd = Random.new
+  array << (rnd.rand(-100..100)).to_i
+}
+puts "\n\nO array #{array.inspect} ordenado é = #{quick_sort(array).inspect}\n\n"
+
+
