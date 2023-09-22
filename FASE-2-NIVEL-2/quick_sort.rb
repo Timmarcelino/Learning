7e4c417e479d7dd7971e9b37bec50e2bc68d565d
TimMarcelino <valentim.marcelino@live.com>
2023-09-22 13:54:40 -0300

    FEAT: Quick Sort Fixes #49

---
+++ FASE-2-NIVEL-2/quick_sort.rb
@@ -0,0 +1,7 @@
+## O processo principal no quickSort é uma partição(). O objetivo das partições é colocar o pivô 
+## (qualquer elemento pode ser escolhido para ser um pivô) em sua posição correta na matriz 
+## classificada e colocar todos os elementos menores à esquerda do pivô e todos os elementos 
+## maiores à direita do pivô .
+## A partição é feita recursivamente em cada lado do pivô depois que o pivô é colocado em sua 
+## posição correta e isso finalmente classifica o array.
+
