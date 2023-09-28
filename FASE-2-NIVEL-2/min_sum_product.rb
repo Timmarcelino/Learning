c8428870d27bb14ea1651a9e75e86c8e0873b64e
Timmarcelino <valentim.marcelino@live.com>
2023-09-28 12:03:27 -0300

    Minimum Sum Fixes #51

--- FASE-2-NIVEL-2/min_sum_product.rb
+++ FASE-2-NIVEL-2/min_sum_product.rb
@@ -2,35 +2,44 @@
 ## permitidas no primeiro array. Em cada modificação, um elemento da primeira matriz pode ser aumentado 
 ## ou diminuído em 2.
 
-def min_sum_prod(arr1, arr2, k)
-    n = arr1.length
-    product_sum = arr1.zip(arr2).sum { |a, b| a * b }
-  
-    k.times do
-      min_val = arr1.min_by(&:abs)
-      min_index = arr1.index(min_val)
-  
-      if min_val >= 0
-        arr1[min_index] -= 2
-      else
-        arr1[min_index] += 2
-      end
-  
-      new_sum = arr1.zip(arr2).sum { |a, b| a * b }
-      
-      # Se a nova soma for menor, atualizamos o valor
-      product_sum = new_sum if new_sum < product_sum
-    end
-  
-    product_sum
+def min_product(a, b, k)
+  n = a.length
+  # Calcula a soma inicial dos produtos dos dois arrays
+  sum = a.zip(b).map { |a, b| a * b }.sum
+
+  # Inicializa o array de resultados
+  res = [sum]
+
+  # Para cada número de modificações
+  (1..k).each do |n|
+    # Encontre o elemento com o menor valor absoluto em a
+    min_val = a.min_by(&:abs)
+
+    # Encontre o índice do elemento com o menor valor absoluto em a
+    min_index = a.index(min_val)
+
+    # Modifique o elemento com o menor valor absoluto em a
+    a[min_index] += 2 if min_val >= 0
+    a[min_index] -= 2 if min_val < 0
+
+    # Recalcula a soma
+    sum += 2 * b[min_index] if min_val >= 0
+    sum -= 2 * b[min_index] if min_val < 0
+
+    # Adicione o novo valor da soma aos resultados
+    res << sum
   end
-  
-  # Teste
-  arr1 = [2, 3, 4, 5, 4]
-  arr2 = [3, 4, 2, 3, 2]
-  k = 3
-  
-  puts "Array1: #{arr1.inspect}"
-  puts "Array2: #{arr2.inspect}"
-  puts "Realizando #{k} modificações, a soma mínima dos produtos dos arrays é: #{min_sum_prod(arr1, arr2, k)}"
+
+  # Retorne o menor valor encontrado
+  return res.min
+end
+
+# Exemplo de uso:
+arr1 = [2, 3, 4, 5, 4]
+arr2 = [3, 4, 2, 3, 2]
+k = 3
+
+puts "\nArray1: #{arr1.inspect}"
+puts "Array2: #{arr2.inspect}"
+puts "Realizando #{k} modificações, a soma mínima dos produtos dos arrays é: #{min_product(arr1, arr2, k)}"
   
