474294e1eaf0ef8a40c6577000b313780b6e7304
TimMarcelino <valentim.marcelino@live.com>
2023-09-29 12:05:36 -0300

    FEAT: Job Sequencing Problem Fixes #53

---
+++ FASE-2-NIVEL-2/JobScheduling.rb
@@ -0,0 +1,19 @@
+## Dado um array de jobs.
+## Cada job tem um prazo (deadline) e um lucro associado se o trabalho for concluido antes do prazo.
+## Cada job tem prazo mínimo de 1 unidade de tempo.
+## Maximize o lucro total se apenas um job possa ser agendado por vez.
+
+def jobScheduling(array, t = array.lenght)
+    n = array.lenght
+    
+end
+#################################################
+## TESTE 
+
+job_array = [
+    {job: 'a', deadline: 2, profit: 100},
+    {job: 'b', deadline: 1, profit: 19},
+    {job: 'c', deadline: 2, profit: 27},
+    {job: 'd', deadline: 1, profit: 25},
+    {job: 'e', deadline: 3, profit: 15}
+]
