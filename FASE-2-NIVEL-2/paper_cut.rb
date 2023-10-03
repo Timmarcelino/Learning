fcf1b2b6218894dbe9713b8e7d3c769a42214a0a
Timmarcelino <valentim.marcelino@live.com>
2023-10-03 19:53:37 -0300

    REFACTOR:  Paper Cut Fixes #57

--- FASE-2-NIVEL-2/paper_cut.rb
+++ FASE-2-NIVEL-2/paper_cut.rb
@@ -2,24 +2,23 @@
 ## Encontre o número de quadrados que podem ser cortados do papel.
 
 def minimumSquare(a, b)
-
-    if a < b   # Colocando o lado maior em a
-        a , b = b , a
-    end
-
-    square = 0
-
-    while b > 0 do
-        square += (a/b).to_i
-        resto =  (a%b).to_i
-        a = b
-        b = resto
-    end
-    return square
+    squares = 0
+    puts "#{a} #{b}"
+    while b > 0 && a > 0 do
+        if a >= b   #  Significa que um quadrado de tamanho 'b x b' cabe no papel.
+            squares += (a / b)  # Adiciona a quantidade de quadrados '(b x b)' que podem ser cortados na direção de 'a'
+            a %= b   # Atualiza o valor de 'a' para ser o papel restante após cortar os quadrados
+        else # Significa que um quadrado de tamanho 'a x a' cabe no papel.
+            squares += (b / a)  # Adiciona a quantidade de quadrados '(a x a)' que podem ser cortados na direção de 'b'
+            b %= a  # Atualiza o valor de 'b' para ser o papel restante após cortar os quadrados
+        end
+        puts "#{a} #{b} -> #{squares}"
+    end 
+    return squares
 end
 ################
 # TESTE
 
-h = 29
-w = 13
+h = 36
+w = 30
 puts "\n\nPodem ser cortados #{minimumSquare(h, w)} quadrados em uma folha de #{h} x #{w}\n\n"
