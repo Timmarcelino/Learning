ca66570892a4c3e5be86378cff6f9f0f88fb4bfa
Timmarcelino <valentim.marcelino@live.com>
2023-10-03 17:27:34 -0300

    TEST: Paper Cut

--- FASE-2-NIVEL-2/paper_cut.rb
+++ FASE-2-NIVEL-2/paper_cut.rb
@@ -2,12 +2,24 @@
 ## Encontre o número de quadrados que podem ser cortados do papel.
 
 def minimumSquare(a, b)
-    
+
+    if a < b   # Colocando o lado maior em a
+        a , b = b , a
+    end
+
+    square = 0
+
+    while b > 0 do
+        square += (a/b).to_i
+        resto =  (a%b).to_i
+        a = b
+        b = resto
+    end
+    return square
 end
 ################
 # TESTE
 
 h = 29
 w = 13
-
-puts minimumSquare(h, w)
+puts "\n\nPodem ser cortados #{minimumSquare(h, w)} quadrados em uma folha de #{h} x #{w}\n\n"
