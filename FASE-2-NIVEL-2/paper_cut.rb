ebba2388f873bf5782c6e99a6b3bc91e5b72e1f7
Timmarcelino <valentim.marcelino@live.com>
2023-10-03 17:18:07 -0300

    FEAT: Paper Cut Fixes #57

---
+++ FASE-2-NIVEL-2/paper_cut.rb
@@ -0,0 +1,13 @@
+## Dado um papel de tamanho A x B. A tarefa é cortar o papel em quadrados de qualquer tamanho. 
+## Encontre o número de quadrados que podem ser cortados do papel.
+
+def minimumSquare(a, b)
+    
+end
+################
+# TESTE
+
+h = 29
+w = 13
+
+puts minimumSquare(h, w)
