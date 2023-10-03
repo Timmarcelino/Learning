930480bbedb3f8d4dab0f1c4f7cb871319a3fcfc
Timmarcelino <valentim.marcelino@live.com>
2023-10-03 00:45:42 -0300

    REFACTOR: fractional_knapsack Finish

--- FASE-2-NIVEL-2/fractional_knapsack.rb
+++ FASE-2-NIVEL-2/fractional_knapsack.rb
@@ -4,7 +4,7 @@
 
 def fractional_knapsack(itens, capacidade)
    
-    itens.sort_by! { |item| - item[:lucro] / item[:peso] } # Classifica os itens em ordem decrescente de lucro
+    itens.sort_by! { |item| - item[:valor] / item[:peso] } # Classifica os itens em ordem decrescente de lucro
 
     total = 0.0
     mochila = []
@@ -12,12 +12,12 @@ def fractional_knapsack(itens, capacidade)
     itens.each do |item|
         if capacidade >= item[:peso] # Se o item couber inteiro na mochila, adicionar
             mochila << item
-            total += item[:lucro]
+            total += item[:valor]
             capacidade -= item[:peso]
         else # Caso contrário adicionar uma fração do item na mochila
             fracao = capacidade.to_f / item[:peso]
-            mochila << {lucro: fracao * item[:lucro], peso: capacidade}
-            total += fracao * item[:lucro]
+            mochila << {valor: fracao * item[:valor], peso: capacidade}
+            total += fracao * item[:valor]
             break # mochila cheia
         end
     end
@@ -26,18 +26,18 @@ def fractional_knapsack(itens, capacidade)
 end
 
 
-### TESTE Hash {lucro: , peso: }
+### TESTE Hash {valor: , peso: }
 
 arr_itens = [
-    {lucro:500, peso:30 }, 
-    #{lucro:60, peso:10 }, 
-    #{lucro:120, peso:30 } 
+    {valor:100, peso:20 }, 
+    {valor:60, peso:10 }, 
+    {valor:120, peso:30 } 
 ] 
-capacidade = 10
+capacidade = 50
 
 mochila, lucro_T = fractional_knapsack(arr_itens, capacidade)
 
-puts "Itens na mochila:"
-#mochila.each {|item| puts "Lucro: #{item[:lucro]}, Peso: #{item[:peso]}"}
+puts "\n\n\nItens na mochila:"
+#mochila.each {|item| puts "Valor: #{item[:valor]}, Peso: #{item[:peso]}"}
 puts mochila.inspect
-puts "Lucro total na mochila: #{lucro_T}"
+puts "Lucro total na mochila: #{lucro_T}\n\n\n"
